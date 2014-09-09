68c81728c9a9e90a8474b245aca38a7482475736
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2014-09-09 23:58:49 +0200

    Finished the port (minus pretty printing).

--- AlgorithmW/Program.fs
+++ AlgorithmW/Program.fs
@@ -1,7 +1,25 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open AlgorithmW.Syntax
+
+let e0 = ELet ("id", EAbs ("x", EVar "x"), EVar "id")
+let e1 = ELet ("id", EAbs ("x", EVar "x"), EApp (EVar "id", EVar "id"))
+let e2 = ELet ("id", EAbs ("x", ELet ("y", EVar "x", EVar "y")), EApp (EVar "id", EVar "id"))
+let e3 = ELet ("id", EAbs ("x", ELet ("y", EVar "x", EVar "y")), EApp (EApp (EVar "id", EVar "id"), ELit (LInt 2)))
+let e4 = ELet ("id", EAbs ("x", EApp (EVar "x", EVar "x")), EVar "id") // Supposed to fail
+let e5 = EAbs ("m", ELet ("y", EVar "m", ELet ("x", EApp (EVar "y", ELit (LBool true)), EVar "x")))
+
+let test e =
+    try 
+        let t = typeInference Map.empty e
+        printfn "%A : %A" e t
+    with TypeError (m, t1, t2) -> 
+        printfn "%A ! %s: %A vs. %A" e m t1 t2
+    printfn ""
+
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    List.map test [e0; e1; e2; e3; e4; e5] |> ignore
+    Console.ReadLine() |> ignore
     0 // return an integer exit code
 
--- AlgorithmW/Syntax.fs
+++ AlgorithmW/Syntax.fs
@@ -8,6 +8,7 @@ type Exp =
     | EVar of string
     | ELit of Lit
     | EApp of Exp * Exp
+    | EAbs of string * Exp
     | ELet of string * Exp * Exp
 
 type Type =
@@ -19,6 +20,8 @@ type Type =
 type Scheme = Scheme of List<string> * Type
 
 type Subst = Map<string, Type>
+
+exception TypeError of string * Type * Type
 
 type Types<'a> = {
     ftv : 'a -> Set<string>;
@@ -44,15 +47,89 @@ let rec typeTypes : Types<Type> = {
         | TFun (t1, t2) -> TFun (typeTypes.apply s t1, typeTypes.apply s t2)
 }
 
-let rec substTypes : Types<Scheme> = {
+let schemeTypes : Types<Scheme> = {
     ftv = fun (Scheme (vars, t)) -> Set.difference (typeTypes.ftv t) (Set.ofList vars)
 
     apply = fun s (Scheme (vars, t)) -> Scheme (vars, typeTypes.apply (List.foldBack Map.remove vars s) t)
 }
 
-let rec listTypes (types : Types<'a>) : Types<List<'a>> = {
+let listTypes (types : Types<'a>) : Types<List<'a>> = {
     ftv = fun l -> List.foldBack Set.union (List.map types.ftv l) Set.empty
 
     apply = fun s l -> List.map (types.apply s) l
 }
 
+let nullSubst : Subst = Map.empty
+let composeSubst (s1 : Subst) (s2 : Subst) : Subst =
+    let s2' : Subst = Map.map (fun _ t -> typeTypes.apply s1 t) s2 
+    List.fold (fun s (k, v) -> Map.add k v s) s1 (Map.toList s2') 
+
+type TypeEnv = Map<string, Scheme>
+
+let typeEnvTypes : Types<TypeEnv> = {
+    ftv = fun env -> (listTypes schemeTypes).ftv (List.map snd (Map.toList env))
+    apply = fun s env -> Map.map (fun _ scheme -> schemeTypes.apply s scheme) env
+}
+
+let generalize env t =
+    let vars = Set.toList (Set.difference (typeTypes.ftv t) (typeEnvTypes.ftv env)) 
+    Scheme (vars, t)
+
+// A minor difference here is that we use effects instead of monads to generate the fresh type variables
+let instantiate newTyVar (Scheme (vars, t)) : Type =
+    let nvars = List.map (fun _ -> newTyVar "a") vars
+    let s = Map.ofList (List.zip vars nvars)
+    typeTypes.apply s t
+
+// A minor difference here is that mgu is pure and doesn't need a monad
+let rec mgu t1 t2 =
+    let varBind u t =
+        if t = TVar u then nullSubst
+        else if Set.contains u (typeTypes.ftv t) then raise (TypeError ("occurs check fails", TVar u, t))
+        else Map.add u t Map.empty
+    match (t1, t2) with
+    | (TFun (l, r), TFun (l', r')) -> 
+        let s1 = mgu l l'
+        let s2 = mgu r r'
+        composeSubst s1 s2
+    | (TVar u, t) | (t, TVar u) -> varBind u t
+    | (TInt, TInt) -> nullSubst
+    | (TBool, TBool) -> nullSubst
+    | _ -> raise (TypeError ("types do not unify", t1, t2))
+
+// A minor difference her is that we use some local state instead of a state monad
+let typeInference env e =
+    let tiSupply = ref 0
+    let newTyVar prefix =
+        let i = !tiSupply
+        tiSupply := i + 1
+        TVar (prefix + i.ToString())
+    let rec ti env e  =
+        match e with
+        | EVar n -> 
+            match Map.tryFind n env with
+            | None -> failwith ("unbound variable" + n.ToString())
+            | Some sigma -> (nullSubst, instantiate newTyVar sigma)
+        | ELit (LBool _) -> (nullSubst, TBool)
+        | ELit (LInt _) -> (nullSubst, TInt)
+        | EAbs (n, e) ->
+            let tv = newTyVar "a"
+            let env' = Map.remove n env
+            let env'' = Map.add n (Scheme ([], tv)) env'
+            let (s1, t1) = ti env'' e
+            (s1, TFun (typeTypes.apply s1 tv, t1))
+        | EApp (e1, e2) ->
+            let tv = newTyVar "a"
+            let (s1, t1) = ti env e1
+            let (s2, t2) = ti (typeEnvTypes.apply s1 env) e2
+            let s3 = mgu (typeTypes.apply s2 t1) (TFun (t2, tv))
+            (composeSubst (composeSubst s3 s2) s1, typeTypes.apply s3 tv)
+        | ELet (x, e1, e2) ->
+            let env' = Map.remove x env
+            let (s1, t1) = ti env e1
+            let t' = generalize (typeEnvTypes.apply s1 env) t1
+            let env'' = Map.add x t' env'
+            let (s2, t2) = ti (typeEnvTypes.apply s1 env'') e2
+            (composeSubst s1 s2, t2)
+    let (s, t) = ti env e
+    typeTypes.apply s t
